7a336603e83f6de774c1c65c2f612ab71df3874e
asingh-unc <asheamar@gmail.com>
2019-03-25 00:51:02 -0400

    work?

--- nanachi.js
+++ nanachi.js
@@ -1,1 +1,10 @@
 //let's do this
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+})
+
+bot_secret_token = "Mzc2MjMzNjc4NTAzOTM2MDEw.D3nuqA.bfWPvFMKA-H6CPf52i7Hv0oUlm0"
+client.login(bot_secret_token)
