d41bbcead371553369ba8b89746a44b04c43f4f4
Amar <asheamar@gmail.com>
2019-05-18 07:08:10 -0400

    witch doctor

--- nanachi.js
+++ nanachi.js
@@ -296,6 +296,16 @@ function processCommand(receivedMessage) {
                 })
                 .catch(console.error);
             }
+            else if(splitCommand[1] === "r4yd?") {
+                hellchannel.join()
+                .then((vconnection) => {
+                    const player = vconnection.playFile("/home/pi/Desktop/nanachi/nanachi-bot/r4yd.mp3");
+                    player.on("end", end => {
+                        hellchannel.leave();
+                    })
+                })
+                .catch(console.error);
+            }
             else {
                 receivedMessage.channel.send("Not a valid command.");
             }
