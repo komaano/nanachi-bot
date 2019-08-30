7c2192c0bd5c9bbe17eccecc2584548960dde631
Amar <asheamar@gmail.com>
2019-08-30 18:30:05 -0400

    weirdchamp2

--- nanachi.js
+++ nanachi.js
@@ -22,7 +22,7 @@ client.on('message', (receivedMessage) => {
     }
 
     else if(receivedMessage.member.id === '125355456653688832') {
-        receivedMessage.react(':weirdchamp:')
+        receivedMessage.react(receivedMessage.guild.emojis.get('617123576398479380'))
         .then(console.log)
         .catch(console.error);
         return;
